ff62bf44fcdb74fe746b5e4bd12e1a7ca9abd56e
Jin DU <telepathymira@gmail.com>
2022-02-03 23:13:17 +0100

    controller add

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,14 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+=begin
+require 'faker'
+
+puts 'Cleaning up database...'
+Movie.destroy_all
+puts 'Database cleaned'
+
+10.times do
+  Movie.create([{
+    title: Faker::Movie.title,
+    overview: Faker::Company.catch_phrase,
+    poster_url: Faker::File.dir,
+    rating: rand(0..10)
+  }])
+end
+
+puts Movie.all
+puts 'movies created!'
+=end
+
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning up database...'
+Movie.destroy_all
+puts 'Database cleaned'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+10.times do |i|
+  puts "importing films from page #{ i + 1 }"
+  movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    poster_base_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: movie["#{poster_base_url}#{movie['backdrop_path']}"],
+      rating: movie['vote_average']
+    )
+  end
+end
+
+puts "Movies created"
